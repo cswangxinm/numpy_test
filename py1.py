b40ebcda2635ddcea0ce1c53b97af0d0da777baa
shane <cswangxinm@163.com>
2013-06-03 19:49:32 +0800

    First commit

---
+++ py1.py
@@ -0,0 +1,6 @@
+from OpenGL.raw.GL.VERSION.GL_1_1 import glLineWidth
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import scipy.optimize as optimize
